b6fb2fcebc375a47500bbc691aaf1c5f4c8728a9
marlon <malba@gbox.ncf.edu.ph>
2024-11-15 11:54:51 +0800

    updated code

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,14 @@
-import './App.css'
-
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+import React from 'react';
 import Home from './Home';
-import About from './About';
-import Contacts from './Contacts';
-import Education from './Education';
-import Hobbies from './Hobbies';
-import Header from './Header';
 
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
 
 function App() {
-
-  return ( 
-    <Router>
-      <div className="App"> 
-        {/* Navigation Links */}
-        <Navbar bg="primary" expand="lg" data-bs-theme="dark">
-          <Container>
-            <Navbar.Brand href="/">My Perosnal Website</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link as={NavLink} to="/home" activeClassName="active-link">Home</Nav.Link>
-              <Nav.Link as={NavLink} to="/About" activeClassName="active-link">About</Nav.Link>
-              <Nav.Link as={NavLink} to="/Contacts" activeClassName="active-link">Contacts</Nav.Link>
-              <Nav.Link as={NavLink} to="/Education" activeClassName="active-link">Education</Nav.Link>
-              <Nav.Link as={NavLink} to="/Hobbies" activeClassName="active-link">Hobbies</Nav.Link>
-            </Nav>
-          </Container>
-        </Navbar>
-
-        {/* Routes for different pages */}
-        <Routes>
-          <Route path="/" element={<Header />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Contacts" element={<Contacts />} />
-          <Route path="/Education" element={<Education />} />
-          <Route path="/Hobbies" element={<Hobbies />} />
-        </Routes>
-      </div>
-    </Router>
-  )
+  return (
+    <div>
+      <Home />
+      <customNavBar />
+    </div>
+  );
 }
 
 export default App;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
+import { Container, Row, Col, Button } from 'react-bootstrap';
+import { FaGithub, FaFacebook, FaInstagram } from 'react-icons/fa';
+import './Home.css';
 
 function Home() {
   return (
-    <div>
-      <h1>Welcome to the Home Component!</h1>
-      <p>This is the Home component content.</p>
+    <div className="hero-section d-flex align-items-center">
+      <Container>
+        <Row className="align-items-center">
+          <Col md={6}>
+            <h1>Hi! I'm Marlon</h1>
+            <h2>I'M A COMPUTER SCIENCE STUDENT</h2>
+            <p>
+              A mediocre student who loves doing average stuff.
+            </p>
+            <Button variant="primary" className="mr-2">Get in Touch</Button>
+            <div className="social-icons mt-3">
+            <a href="https://github.com/marlon-cmd" target="_blank" rel="noopener noreferrer">
+              <FaGithub size={30} className="mr-3" />
+            </a>
+            <a href="https://web.facebook.com/marlon.alba.758" target="_blank" rel="noopener noreferrer">
+              <FaFacebook size={30} className="mr-3" />
+              </a>
+              <a href="https://www.instagram.com/prec8.marlon/" target="_blank" rel="noopener noreferrer">
+              <FaInstagram size={30} />
+              </a>
+            </div>
+          </Col>
+          <Col md={6}>
+            <img src="https://lh3.googleusercontent.com/a/ACg8ocIatKlTZz9e4by1usyEOmA6ABIfKpxns5_0WZKqTx5mTJL5pJI=s309-c-no" alt="Jenna Ortega" className="profile-img" />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
